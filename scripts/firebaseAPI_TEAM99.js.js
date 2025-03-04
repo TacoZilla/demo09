b9703da20fa1f6799368a47b620848324b071a03
Luka Poledica <lukapoledica26@gmail.com>
2025-03-04 09:27:36 -0800

    added new script

---
+++ scripts/firebaseAPI_TEAM99.js.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDW0xiTMVIup9pwnNpEiqGu8m8XQd3_xVk",
+  authDomain: "comp202510-demo-07.firebaseapp.com",
+  projectId: "comp202510-demo-07",
+  storageBucket: "comp202510-demo-07.firebasestorage.app",
+  messagingSenderId: "838566005246",
+  appId: "1:838566005246:web:95c900325a3337b84458af"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
